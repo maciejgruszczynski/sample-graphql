7e8729d04b845305b616b092d4bdc671a9afc33f
Maciej Gruszczyński <maciej.gruszczynski@gmail.com>
2021-04-15 00:09:57 +0200

    +rpsec, factory bot, models Project, Task, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+if Rails.env.development?
+  ActiveRecord::Migration.say_with_time('Creating Projects and tasks') do
+    5.times do |i|
+      project = Project.new(name: "Project_#{i}", description: "Description of the project_#{i}")
+      5.times do |i|
+        project.tasks.build(title: "Task_#{i}", description: "Description of the task_#{i}")
+      end
+      project.save!
+    end
+  end
+end
